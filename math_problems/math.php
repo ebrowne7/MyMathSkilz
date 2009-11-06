82133a9d012695ad835221fec000a7fd003427a6
Ed Browne <ebrowne7@gmail.com>
2009-11-06 15:53:20 -0500

    Added images and moved them into the code rotation

--- math_problems/math.php
+++ math_problems/math.php
@@ -57,15 +57,17 @@ function gen_new_problem($arg_min,$arg_max,$oper){
 }
 
 function display_math_problem($argument1,$oper,$argument2){
+   $img = array("apple.gif","grapes.gif", "penguin.gif", "puimpin.gif", "vegetable-01.gif", "vegetable-02.gif");
+   $randimg = $img[rand(0,5)];
 
    echo "<table>"
 	."<tr><td> ";
    for($i = 0;$i< $argument1;$i++){
-      echo "<img src=img/penguin.gif height=50 width=50>";
+      echo "<img src=img/$randimg >";
    }
    echo "</td><td></td><td>";
    for($i = 0;$i< $argument2;$i++){
-      echo "<img src=img/penguin.gif height=50 width=50>";
+      echo "<img src=img/$randimg >";
    }
    echo "</td><td></td></tr><tr><td align=center>";
    echo "$argument1<input type=hidden name=argument1 value=$argument1></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> $argument2  <input type=hidden name=argument2 value=$argument2></td><td>=<input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";

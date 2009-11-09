36983b079f54659605ecd53510661b7621dfe5fe
Ed Browne <ebrowne7@gmail.com>
2009-11-08 19:21:22 -0500

    Added Subtraction images and loop added ul list to index

--- math_problems/math.php
+++ math_problems/math.php
@@ -10,10 +10,10 @@ function check_problem($argument1, $oper, $argument2, $arg_min, $arg_max){
 	  }
           break;
        case '-':
-          if (($argument1 - $argument2) > $arg_max && ($argument - $argument2) < $arg_min){
-            $result=false;
+          if ((($argument1 - $argument2) < $arg_max) && (($argument1 - $argument2) > $arg_min)){
+            $result=true;
           }else{
-             $result=true;
+             $result=false;
           }
           break;
 	case '*':
@@ -98,29 +98,62 @@ function display_math_problem($argument1,$oper,$argument2){
    }
 
    echo "<table width=440px border=0 valign=top cellpadding=0 cellspacing=0>
-	<tr valign=top><td > ";
+	<tr valign=top>";
 
-   if($argument1 == 0 ){
-      echo "<img src='img/blank.gif'>";
+   if($oper == '+'){
+      echo "<td > ";
+   }else{
+      echo "<td colspan =5>";
    }
-   for($i = 0;$i< $argument1;$i++){
-      echo "<img src='img/$randimg' >";
-      if (($i !== 0) && (($i+1) % 5) ==0){ 
-         echo "<br/>";
+   
+
+      if($argument1 == 0 ){
+         echo "<img src='img/blank.gif'>";
       }
-   }
-   echo "</td><td ><img src='img/blank.gif'></td><td >";
-   if($argument2 == 0 ){
-      echo "<img src='img/blank.gif'>";
-   }
-   for($i = 0;$i< $argument2;$i++){
-      echo "<img src='img/$randimg' >";
-      if (($i !== 0) && (($i+1) % 5) ==0){
-         echo "<br/>";
+      if($oper == '+'){
+      for($i = 0;$i< $argument1;$i++){
+         echo "<img src='img/add/$randimg' >";
+         if (($i !== 0) && (($i+1) % 5) ==0){ 
+            echo "<br/>";
+         }
       }
-   }
+      }else{
+         $result = $argument1 - $argument2;
+         for($i=0; $i< $result; $i++){
+             echo "<img src='img/add/$randimg' >";
+            if (($i !== 0) && (($i+1) % 5) ==0){ 
+               echo "<br/>";
+            }
+         }
+         for($i=0; $i< $argument2; $i++){
+             echo "<img src='img/sub/$randimg' >";
+            if (($i !== 0) && (($i+1) % 5) ==0){ 
+               echo "<br/>";
+            }
+         }
+         
+
+      }
+      echo "</td><td ><img src='img/blank.gif'></td><td >";
+      if($argument2 == 0 ){
+         echo "<img src='img/blank.gif'>";
+      }
+      if($oper == '+'){
+         for($i = 0;$i< $argument2;$i++){
+            echo "<img src='img/add/$randimg' >";
+            if (($i !== 0) && (($i+1) % 5) ==0){
+               echo "<br/>";
+            }
+         }
+      }
+
+if($oper == '+'){
    echo "</td><td >&nbsp;</td><td>&nbsp;</td></tr><tr ><td align=center>
    $argument1<input type=hidden name=argument1 value=$argument1></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> $argument2  <input type=hidden name=argument2 value=$argument2></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
+}else{
+   echo "</td></tr><tr ><td align=center>
+   $argument1<input type=hidden name=argument1 value=$argument1></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> $argument2  <input type=hidden name=argument2 value=$argument2></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
+}
 
 }
 
@@ -167,6 +200,18 @@ function verifyResult(){
                }
             break;
 	 case '-':
+               if($_SESSION['num_questions'] > 0){
+                  if($_POST['argument1'] - $_POST['argument2'] == $_POST['result']){
+                     $_SESSION['correct']++;
+                     $_SESSION['iscorrect'] = true;
+                  }else{
+                     $_SESSION['incorrect']++;
+                     $_SESSION['iscorrect'] = false;
+                  } 
+                  #Count down the questions as they are answered.
+                  $_SESSION['num_questions']--;
+               }
+            
             break;
          default:
             break;

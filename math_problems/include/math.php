655151c35ecd1b748fd932db0a6894ca03d6d439
Ed Browne <ebrowne7@gmail.com>
2009-12-01 11:31:45 -0500

    Changed word problems to not show valuse

--- math_problems/include/math.php
+++ math_problems/include/math.php
@@ -75,6 +75,80 @@ function display_word_problem($argument1,$oper,$argument2,$nf=false){
    preg_match('/[A-z]+/',$img_name,$matches);
    $img_name = $matches[0];
 
+   echo "<table width=440px border=0 valign=top cellpadding=0 cellspacing=0>
+	<tr valign=top>";
+
+   if($oper == '+'){
+      echo "<td > ";
+   }else{
+      if($nf){
+      echo "<td colspan =5>";
+      }else{
+      echo "<td >";
+      }
+   }
+
+   if($nf){
+   if(!$_SESSION['noimage']){
+      if($argument1 == 0 ){
+         echo "<img src='img/blank.gif'>";
+      }
+      if($oper == '+'){
+      for($i = 0;$i< $argument1;$i++){
+         echo "<img src='img/add/$randimg' >";
+         if (($i !== 0) && (($i+1) % 5) ==0){ 
+            echo "<br/>";
+         }
+      }
+      }else{
+         $result = $argument1 - $argument2;
+         for($i=0; $i< $result; $i++){
+             echo "<img src='img/add/$randimg' >";
+            if (($i !== 0) && (($i+1) % 5) ==0){ 
+               echo "<br/>";
+            }
+         }
+         for($i=0; $i< $argument2; $i++){
+             echo "<img src='img/sub/$randimg' >";
+            if (($i !== 0) && (($i+1) % 5) ==0){ 
+               echo "<br/>";
+            }
+         }
+         
+
+      }
+      echo "</td><td ><img src='img/blank.gif'></td><td >";
+      if($argument2 == 0 ){
+         echo "<img src='img/blank.gif'>";
+      }
+      if($oper == '+'){
+         for($i = 0;$i< $argument2;$i++){
+            echo "<img src='img/add/$randimg' >";
+            if (($i !== 0) && (($i+1) % 5) ==0){
+               echo "<br/>";
+            }
+         }
+      }
+   }else{
+      echo "</td><td ><img src='img/blank.gif'></td><td >";
+   }
+   }else{
+   echo "</td><td>$argument1</td></tr><tr><td>$oper</td><td>$argument2</td></tr><tr><td colspan=2><hr></td></tr><td colspan=2>";
+   }
+if($nf){
+if($oper == '+'){
+   echo "</td><td >&nbsp;</td><td>&nbsp;</td></tr><tr ><td align=center>
+   <input type=text size=5 name=argument1 ></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> <input type=text name=argument2 size=5></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
+}else{
+   echo "</td></tr><tr ><td align=center>
+   <input type=text name=argument1 size=5></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> <input type=text name=argument2 size=5></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
+}
+}else{
+   echo " <input type=text name=argument1 size=5><input type=hidden name=oper value=$oper><input type=text name=argument2 size=5><input type=text name='result' value='' size='5'> </td></tr></table><br><input type=submit value='submit'>";
+
+}
+
+
 }
 
 function display_math_problem($argument1,$oper,$argument2,$nf=true){

2a5807f692d4715d7b6d2f4b3283c5e58d7e8003
Ed Browne <ebrowne7@gmail.com>
2009-12-01 20:04:52 -0500

    Added word problems

--- math_problems/include/math.php
+++ math_problems/include/math.php
@@ -41,6 +41,14 @@ function check_problem($argument1, $oper, $argument2, $arg_min, $arg_max){
 
 
 function gen_new_problem($arg_min,$arg_max,$oper,$doubles=false,$nf=true,$word=false){
+   if($word){
+      $_SESSION['wordproblem']=true;
+   }else{
+      if(isset($_SESSION['wordproblem'])){
+         unset($_SESSION['wordproblem']);  
+      }
+
+   }
    $argument1 = get_new_number($arg_min,$arg_max);
    if (!$doubles){
       $argument2 = get_new_number($arg_min,$arg_max);
@@ -67,6 +75,8 @@ function gen_new_problem($arg_min,$arg_max,$oper,$doubles=false,$nf=true,$word=f
 }
 
 function display_word_problem($argument1,$oper,$argument2,$nf=false){
+   $_SESSION['argument1']=$argument1;
+   $_SESSION['argument2']=$argument2;
    $img = get_image($oper);
    $randimg = $img[rand(0,count($img)-1)];
    $info = pathinfo($randimg);
@@ -74,8 +84,12 @@ function display_word_problem($argument1,$oper,$argument2,$nf=false){
  
    preg_match('/[A-z]+/',$img_name,$matches);
    $img_name = $matches[0];
+   if ($argument1 > 1){
+      $img_name.="s";
+   }
 
-   echo "<table width=440px border=0 valign=top cellpadding=0 cellspacing=0>
+   echo "There are $argument1 $img_name in the room and $argument2 more come in. How many $img_name in all?
+   <table width=440px border=0 valign=top cellpadding=0 cellspacing=0>
 	<tr valign=top>";
 
    if($oper == '+'){
@@ -138,16 +152,15 @@ function display_word_problem($argument1,$oper,$argument2,$nf=false){
 if($nf){
 if($oper == '+'){
    echo "</td><td >&nbsp;</td><td>&nbsp;</td></tr><tr ><td align=center>
-   <input type=text size=5 name=argument1 ></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> <input type=text name=argument2 size=5></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
+   <input type=text size=5 name=rargument1 ></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> <input type=text name=rargument2 size=5></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
 }else{
    echo "</td></tr><tr ><td align=center>
-   <input type=text name=argument1 size=5></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> <input type=text name=argument2 size=5></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
+   <input type=text name=rargument1 size=5></td><td align=center> $oper <input type=hidden name=oper value=$oper></td><td align=center> <input type=text name=rargument2 size=5></td><td>=</td><td><input type=text name='result' value='' size='5'> </td></tr></table><input type=submit value='submit'>";
 }
 }else{
-   echo " <input type=text name=argument1 size=5><input type=hidden name=oper value=$oper><input type=text name=argument2 size=5><input type=text name='result' value='' size='5'> </td></tr></table><br><input type=submit value='submit'>";
+   echo " <input type=text name=rargument1 size=5><input type=hidden name=oper value=$oper><input type=text name=rargument2 size=5><input type=text name='result' value='' size='5'> </td></tr></table><br><input type=submit value='submit'>";
 
 }
-
 
 }
 
@@ -240,23 +253,49 @@ if($oper == '+'){
 }
 
 function verifyResult(){
-   if(isset($_POST['argument1'])){
+   if(isset($_POST['argument1']) || isset($_POST['rargument1'])){
       switch($_POST['oper']){
          case '+': 
                if($_SESSION['num_questions'] > 0){
-                  if($_POST['argument1'] + $_POST['argument2'] == $_POST['result']){
-                     $_SESSION['correct']++;
-                     $_SESSION['iscorrect'] = true;
+                  if(isset($_SESSION['wordproblem'])){
+                     if(($_SESSION['argument1'] == $_POST['rargument1']) && ($_SESSION['argument2'] == $_POST['rargument2'])&& (($_SESSION['argument1'] + $_SESSION['argument2']) == $_POST['result'])){
+                        $_SESSION['correct']++;
+                        $_SESSION['iscorrect'] = true;
+                     }else{
+                        $_SESSION['incorrect']++;
+                        $_SESSION['iscorrect'] = false;
+                     }
+                     #Count down the questions as they are answered.
+                     $_SESSION['num_questions']--;
+
                   }else{
-                     $_SESSION['incorrect']++;
-                     $_SESSION['iscorrect'] = false;
-                  } 
+                     if($_POST['argument1'] + $_POST['argument2'] == $_POST['result']){
+                        $_SESSION['correct']++;
+                        $_SESSION['iscorrect'] = true;
+                     }else{
+                        $_SESSION['incorrect']++;
+                        $_SESSION['iscorrect'] = false;
+                     } 
                   #Count down the questions as they are answered.
                   $_SESSION['num_questions']--;
+                  }
                }
             break;
 	 case '-':
                if($_SESSION['num_questions'] > 0){
+                  if(isset($_SESSION['wordproblem'])){
+                     if(($_SESSION['argument1'] == $_POST['rargument1']) && ($_SESSION['argument2'] == $_POST['rargument2'])&& (($_SESSION['argument1'] - $_SESSION['argument2']) == $_POST['result'])){
+                        $_SESSION['correct']++;
+                        $_SESSION['iscorrect'] = true;
+                     }else{
+                        $_SESSION['incorrect']++;
+                        $_SESSION['iscorrect'] = false;
+                     }
+                     #Count down the questions as they are answered.
+                     $_SESSION['num_questions']--;
+
+                  }else{
+
                   if($_POST['argument1'] - $_POST['argument2'] == $_POST['result']){
                      $_SESSION['correct']++;
                      $_SESSION['iscorrect'] = true;
@@ -266,6 +305,7 @@ function verifyResult(){
                   } 
                   #Count down the questions as they are answered.
                   $_SESSION['num_questions']--;
+                  }
                }
             
             break;
